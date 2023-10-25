a1687cead3e336fc9354608183226844594b9964
kuldip2002 <dev.kuldipmathukiya@gmail.com>
2023-10-25 15:48:50 +0530

    layout created

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,2 +1,14 @@
-const Navigation = () => {};
+import { Container, Row, Col } from "react-bootstrap";
+
+const Navigation = () => {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h1>This is Navigation</h1>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 export default Navigation;
