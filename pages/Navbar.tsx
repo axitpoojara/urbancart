240da851d63e06191770a961b2b04d2bb0eae160
kuldip2002 <dev.kuldipmathukiya@gmail.com>
2023-10-25 15:51:31 +0530

    components imported

--- pages/Navbar.tsx
+++ pages/Navbar.tsx
@@ -1,6 +1,12 @@
 import Header from "@/components/Header";
+import Navigation from "@/components/Navigation";
 
 const Navbar = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Navigation />
+    </>
+  );
 };
 export default Navbar;
