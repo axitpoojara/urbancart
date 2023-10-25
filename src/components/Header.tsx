586495b7d0095cfcdb010ae8aca6c56ce8721802
kuldip2002 <dev.kuldipmathukiya@gmail.com>
2023-10-25 15:43:13 +0530

    Header component created

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,2 +1,14 @@
-const Header = () => {};
+import { Col, Container, Row } from "react-bootstrap";
+
+const Header = () => {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h2>This is Header</h2>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 export default Header;
